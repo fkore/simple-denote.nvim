f14e9a5822d9c1f1b83be88c396a0d4cbb6a2850
Kore Francis <frnjenga@frnjengax1dev>
2024-06-05 13:11:13 +0300

    feat: resolve bug with multiple prompts

--- lua/simple-denote/api.lua
+++ lua/simple-denote/api.lua
@@ -3,58 +3,64 @@ local M = {}
 local internal = require("simple-denote.internal")
 
 function M.splitspace(str)
-  local chunks = {}
-  for substring in str:gmatch("%S+") do
-    table.insert(chunks, #chunks, substring)
-  end
-  return chunks
+	local chunks = {}
+	for substring in str:gmatch("%S+") do
+		table.insert(chunks, #chunks, substring)
+	end
+	return chunks
 end
 
 ---@param options table
 ---@param name string|nil
 ---@param tags table|nil
 function M.note(options, name, tags)
-  if not name then
-    vim.ui.input({ prompt = "Note title: " }, function(input)
-      name = input
-    end)
-  end
-  if not tags then
-    vim.ui.input({ prompt = "Tags: " }, function(input)
-      tags = input
-    end)
-  end
-  internal.note(options, name, tags)
+	if not name then
+		vim.ui.input({ prompt = "Note title: " }, function(input)
+			name = input
+			if not tags then
+				vim.ui.input({ prompt = "Tags: " }, function(input)
+					tags = input
+					internal.note(options, name, tags)
+				end)
+			end
+		end)
+	else
+		internal.note(options, name, tags)
+	end
 end
 
 ---@param options table
 ---@param filename string|nil
 ---@param new_title string|nil
-function M.retitle(options, filename, title)
-  if not filename then
-    filename = vim.fn.expand("%")
-  end
-  if not title then
-    vim.ui.input({ prompt = "New title: " }, function(input)
-      title = input
-    end)
-  end
-  internal.retitle(options, filename, title)
+function M.retitle(options, filename, new_title)
+	if not filename then
+		filename = vim.fn.expand("%")
+	end
+	if not new_title then
+		vim.ui.input({ prompt = "New title: " }, function(input)
+			new_title = input
+			internal.retitle(options, filename, new_title)
+		end)
+	else
+		internal.retitle(options, filename, new_title)
+	end
 end
 
 ---@param options table
 ---@param filename string|nil
 ---@param new_tags table|nil
-function M.retag(options, filename, tags)
-  if not filename then
-    filename = vim.fn.expand("%")
-  end
-  if not tags then
-    vim.ui.input({ prompt = "New tags: " }, function(input)
-      tags = input
-    end)
-  end
-  internal.retag(options, filename, tags)
+function M.retag(options, filename, new_tags)
+	if not filename then
+		filename = vim.fn.expand("%")
+	end
+	if not new_tags then
+		vim.ui.input({ prompt = "New tags: " }, function(input)
+			new_tags = input
+			internal.retag(options, filename, new_tags)
+		end)
+	else
+		internal.retag(options, filename, new_tags)
+	end
 end
 
 return M
